74d6e7fded5bc5f46c174489029885a0942595b9
mjdude <mo@syncspot.net>
2017-07-24 13:42:31 -0700

    basic series

--- pandas-basic.py
+++ pandas-basic.py
@@ -9,3 +9,16 @@ d = {'a' : 10, 'b' : 20, 'c' : 30}
 print(pd.Series(data = my_data))
 print(pd.Series(data = my_data, index=labels))
 
+# passing in a numpy array into panda series is essentially the same as passing
+# passin in a python list
+
+print( pd.Series(arr, labels))
+
+# Panda series can even hold functions or references to functions
+ser1 = pd.Series([1,2,3,4], ['USA', 'Germany', 'USSR', 'Japan'])
+
+print(ser1)
+
+ser2 = pd.Series([1,2,5,4], ['USA','Germany', 'Italy', 'Japan'])
+
+print(ser2)

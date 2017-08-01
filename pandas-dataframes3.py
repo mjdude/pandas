2849b76332262352d3b76e08167415e0c0e85fdd
mjdude <mo@syncspot.net>
2017-07-31 22:29:59 -0700

    added dataframes #3 examples

---
+++ pandas-dataframes3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+# Index levels
+
+outside = ['G1', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+print(hier_index)
+

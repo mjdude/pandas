fe4513d84bf9d6a1552b7ae789c4834b725dc965
mjdude <mo@syncspot.net>
2017-07-31 22:17:03 -0700

    added dataframe selection

--- pandas-dataframes2.py
+++ pandas-dataframes2.py
@@ -9,3 +9,24 @@ df = pd.DataFrame(randn(5,4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'] )
 
 print(df)
 
+booldf = df > 0
+
+print(booldf)
+
+print(df[df>0])
+
+print(df['W'] > 0)
+
+print(df[df['W'] > 0])
+
+# This ...
+
+resultdf = df[df['W'] > 0]
+print('resultdf X ')
+print(resultdf['X'])
+
+# is the same as this
+
+print('result df in one step')
+
+print(df[df['W'] > 0]['X'])
